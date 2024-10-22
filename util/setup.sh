4933b677d55a54708d8ff607429ef8762a07877b
Regfare <>
2024-10-22 14:13:44 +0200

    Added comments to setup script

--- util/setup.sh
+++ util/setup.sh
@@ -4,6 +4,8 @@ cd metacg
 mkdir build
 mkdir install
 cd build
+#This needs CMake 3.24 or newer (currently not enforced)
+#It this fails on can delete the policy command and reconfigure
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=../install ..
 make install -j
 
@@ -14,5 +16,6 @@ cd gencc
 git switch llvm18
 mkdir build
 cd build
+#This needs LLVM 18
 cmake -Dmetacg_DIR=../../metacg/install/lib/cmake/metacg -Dspdlog_DIR=../../metacg/install/lib/cmake/spdlog -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j
